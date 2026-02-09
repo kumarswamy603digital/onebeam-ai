c7a5234a2a97f5d7181160cf536f2c71801c81d5
kumar swamy R Y <kumarswamyry789@gmail.com>
2026-02-09 15:20:57 +0530

    Update command.tsx

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import { type DialogProps } from "@radix-ui/react-dialog";
-import { Command as CommandPrimitive } from "cmdk";
-import { Search } from "lucide-react";
-
+  import * as React from "react";
+   import { type DialogProps } from "@radix-ui/react-dialog";
+ import { Command as CommandPrimitive } from "cmdk";
+   import { Search } from "lucide-react";
+   
 import { cn } from "@/lib/utils";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 
